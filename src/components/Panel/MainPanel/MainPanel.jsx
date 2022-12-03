a715fc76fa3043d0e79e0e57b54490b54e58d967
Andres Sanabria <sanabria80.andres@gmail.com>
2022-12-03 17:48:21 -0500

    copy to clipboard and react hot toast added

--- src/components/Panel/MainPanel/MainPanel.jsx
+++ src/components/Panel/MainPanel/MainPanel.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import './MainPanel.scss'
 import {motion} from 'framer-motion';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import toast, { Toaster } from 'react-hot-toast';
 
-const MainPanel = ({ operation,  }) => {
-
+const MainPanel = ({ operation }) => {
     return (
-      	<motion.input 
-			type="text" 
-			className='main-panel' 
-			value={operation === '' ? "0" : operation} 
-			readOnly
-		/>
+		<>
+			<CopyToClipboard text={operation}>
+				<motion.input 
+					onClick={() => toast.success("Copied text!")}
+					type="text" 
+					className='main-panel' 
+					value={operation === '' ? "0" : operation} 
+					readOnly
+				/>
+			</CopyToClipboard>
+			<Toaster position="top-center" reverseOrder={false}/>
+		</>
     )
 }
-
 export default MainPanel
