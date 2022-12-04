5a4fc53fe6e73789245bc39a202a7f98fdea3512
Andres Sanabria <sanabria80.andres@gmail.com>
2022-12-03 21:09:41 -0500

    light mode default and margin to modal keypad

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.scss'
 export const ThemeContext = createContext(null);
 
 function App() {
-	const [theme, setTheme] = useState("dark"); 
+	const [theme, setTheme] = useState("light"); 
 	const toggleTheme = () => setTheme(curr => curr === "light" ? "dark" : "light");
 	const data = { theme, toggleTheme }
 	return (
